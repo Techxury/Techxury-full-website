93263df17936fa9727a80a7a167aea7813893ac5
Techxury <techxury@gmail.com>
2023-11-24 19:07:22 +0530

    update popular courses

--- js/script.js
+++ js/script.js
@@ -1,91 +1,89 @@
-// Sample course data
-const courses = [
-    {
-      title: "Web Development",
-      category: "web",
-      image:
-        "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      description:
-        "Learn the fundamentals of web development using HTML, CSS, and JavaScript.",
-    },
-    {
-      title: "Data Science",
-      category: "data",
-      image:
-        "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      description:
-        "Explore the world of data science with hands-on projects and real-world applications.",
-    },
-    {
-      title: "ai",
-      category: "ai",
-      image:
-        "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      description:
-        "Learn the fundamentals of web development using HTML, CSS, and JavaScript.",
-    },
-    {
-      title: "Full stack web development",
-      category: "web",
-      image:
-        "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      description:
-        "Learn the fundamentals of web development using HTML, CSS, and JavaScript.",
-    },
-    {
-      title: "Data Science",
-      category: "data",
-      image:
-        "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      description:
-        "Explore the world of data science with hands-on projects and real-world applications.",
-    },
-    {
-      title: "ai",
-      category: "ai",
-      image:
-        "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      description:
-        "Learn the fundamentals of web development using HTML, CSS, and JavaScript.",
-    },
-    // Add more courses as needed
-  ];
+// // Sample course data
+// const courses = [
+//     {
+//       title: "Web Development",
+//       category: "web",
+//       image:
+//         "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       description:
+//         "Learn the fundamentals of web development using HTML, CSS, and JavaScript.",
+//     },
+//     {
+//       title: "Data Science",
+//       category: "data",
+//       image:
+//         "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       description:
+//         "Explore the world of data science with hands-on projects and real-world applications.",
+//     },
+//     {
+//       title: "ai",
+//       category: "ai",
+//       image:
+//         "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       description:
+//         "Learn the fundamentals of web development using HTML, CSS, and JavaScript.",
+//     },
+//     {
+//       title: "Full stack web development",
+//       category: "web",
+//       image:
+//         "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       description:
+//         "Learn the fundamentals of web development using HTML, CSS, and JavaScript.",
+//     },
+//     {
+//       title: "Data Science",
+//       category: "data",
+//       image:
+//         "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       description:
+//         "Explore the world of data science with hands-on projects and real-world applications.",
+//     },
+//     {
+//       title: "ai",
+//       category: "ai",
+//       image:
+//         "https://images.pexels.com/photos/12829984/pexels-photo-12829984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       description:
+//         "Learn the fundamentals of web development using HTML, CSS, and JavaScript.",
+//     },
+//     // Add more courses as needed
+//   ];
 
-  // Function to filter courses based on category
-  function filterCourses(category) {
-    const filteredCourses =
-      category === "all"
-        ? courses
-        : courses.filter((course) => course.category === category);
-    displayCourses(filteredCourses);
-  }
+//   // Function to filter courses based on category
+//   function filterCourses(category) {
+//     const filteredCourses =
+//       category === "all"
+//         ? courses
+//         : courses.filter((course) => course.category === category);
+//     displayCourses(filteredCourses);
+//   }
 
-  // Function to display courses
-  function displayCourses(coursesToShow) {
-    const courseContainer = document.getElementById("courseContainer");
-    courseContainer.innerHTML = "";
+//   // Function to display courses
+//   function displayCourses(coursesToShow) {
+//     const courseContainer = document.getElementById("courseContainer");
+//     courseContainer.innerHTML = "";
 
-    coursesToShow.forEach((course) => {
-      const courseCard = document.createElement("div");
-      courseCard.className = "course-card";
+//     coursesToShow.forEach((course) => {
+//       const courseCard = document.createElement("div");
+//       courseCard.className = "course-card";
 
-      courseCard.innerHTML = `
-              <img src="${course.image}" alt="${course.title}">
-              <div class="course-info">
-                  <h2>${course.title}</h2>
-                  <p>${course.description}</p>
-                  <button class="btn_Exp">Explore</button>
-              </div>
-          `;
+//       courseCard.innerHTML = `
+//               <img src="${course.image}" alt="${course.title}">
+//               <div class="course-info">
+//                   <h2>${course.title}</h2>
+//                   <p>${course.description}</p>
+//                   <button class="btn_Exp">Explore</button>
+//               </div>
+//           `;
 
-      courseContainer.appendChild(courseCard);
-    });
-  }
+//       courseContainer.appendChild(courseCard);
+//     });
+//   }
 
-  // Initial display of all courses
-  displayCourses(courses);
-
-
+//   // Initial display of all courses
+//   displayCourses(courses);
 
 
 
@@ -95,9 +93,17 @@ const courses = [
 
 
 
-  var typed = new Typed('.second_text', {
-    strings: ['Web Devlopment', 'Data Science', 'Stock Marketing', 'Game Developer'],
-    typeSpeed: 150,
-    backspeed: 150,
-    loop: true
-  });
+var typed = new Typed('.second_text', {
+  strings: ['Web Devlopment', 'Data Science', 'Stock Marketing', 'Game Developer'],
+  typeSpeed: 150,
+  backspeed: 150,
+  loop: true
+});
+
+
+var typed = new Typed('#ft_fol_move', {
+  strings: ['Instagram', 'facebook', 'Twitter'],
+  typeSpeed: 150,
+  backspeed: 150,
+  loop: true
+});
